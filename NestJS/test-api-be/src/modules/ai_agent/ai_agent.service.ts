095833ab4657519a1749cba35446ff087eccaa30
Thienphuc <mac@MacBook-Air-cua-MAC.local>
2025-10-02 22:28:44 +0700

    fix structure and add ai agent

--- NestJS/test-api-be/src/modules/ai_agent/ai_agent.service.ts
+++ NestJS/test-api-be/src/modules/ai_agent/ai_agent.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class AiAgentService {}
+export class AiAgentService {
+    private readonly aiUrl = "http://python-ai:8000/api";
+    async generateQuestions(text: string) {
+        const res = await axios.post(`${this.aiUrl}/generate-questions`, { text });
+        return res.data;
+      }
+    
+      async reviewExam(examId: number) {
+        const res = await axios.post(`${this.aiUrl}/review-exam`, { examId });
+        return res.data;
+      }
+}
